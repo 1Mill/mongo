43e44a7f8260ca45ac6ff814d985c18a3eca9fba
1Mill <erik.ekberg@outlook.com>
2021-09-30 14:44:22 -0700

    Scaffold class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello world!')
+const { MongoClient } = require('mongodb')
+const { fetchNodeEnv } = require('./fetchNodeEnv')
+
+class Mongo {
+	constructor({
+		db = fetchNodeEnv('MILL_MONGO_DB'),
+		options = {},
+		uri = fetchNodeEnv('MILL_MONGO_URI'),
+	}) {
+		// * Inputs
+		this._dbName = db
+		this._options = options
+		this._uri = uri
+
+		// * State management
+		this._client = undefined
+		this._db = undefined
+	}
+
+	async connect() {
+		if (typeof this._client === 'undefined') {
+			this._client = await new MongoClient(this._uri, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				...this._options,
+			}).connect()
+		}
+
+		if (typeof this._db === 'undefined') {
+			this._db = this._client.db(this._dbName)
+		}
+
+		return { client: this._client, db: this._db }
+	}
+}
+
+module.exports = { Mongo }
