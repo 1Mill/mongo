12293a607f809e0f1e825cb859b215fbb61bec0d
1Mill <erik.ekberg@outlook.com>
2021-10-08 12:14:07 -0700

    Remove unnessesary state

--- src/index.js
+++ src/index.js
@@ -13,25 +13,22 @@ class Mongo {
 		this.uri = uri
 
 		// * State management
-		this.client = undefined
 		this.clientPromise = undefined
 	}
 
 	async connect() {
 		if (typeof this.clientPromise === 'undefined') {
-			// * https://docs.atlas.mongodb.com/best-practices-connecting-from-aws-lambda/
-			this.client = new MongoClient(this.uri, {
+			this.clientPromise = new MongoClient(this.uri, {
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
 				...this.options,
-			})
-			this.clientPromise = this.client.connect()
+			}).connect()
 		}
 
-		this.client = await this.clientPromise
+		const client = await this.clientPromise
+		const db = client.db(this.dbName)
 
-		const db = this.client.db(this.dbName)
-		return { client: this.client, db }
+		return { client, db }
 	}
 }
 
